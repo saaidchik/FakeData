7484535193cd7844be2b52378263d89837b8c6da
said <saidkhokim20@gmail.com>
2023-06-16 18:10:13 +0500

    init

---
+++ functions/index.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import serverless from 'serverless-http'
+import Cartoons from '../route/Cartoons.mjs'
+
+const app = express();
+
+app.get('/', (req, res ) => {
+    res.send('Welcome');
+});
+
+app.use('/cartoons', Cartoons);
+
+const serverlessApp = serverless(app);
+
+export const handler = async (event, context) => {
+    const result = await serverlessApp(event, context);
+    return result;
+};
